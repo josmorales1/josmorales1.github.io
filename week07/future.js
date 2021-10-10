bd33473dbcff2e7c5f776948544c6ae411702fdd
Jose Morales <josgerardomorales1@aol.com>
2021-10-10 18:41:52 -0500

    Update js

--- week07/future.js
+++ week07/future.js
@@ -3,11 +3,12 @@ var city= ['Dallas', 'London', 'Madrid',"Austin"];
 var spouse= ["Amanda",'Elizabeth',"Jennifer","Amber"];
 var numKids = [4, 2, 7, 13]
 
-
 var x = Math.floor(Math.random() * 4)
 
-function fortune(job, city, spouse, numKids) {
+function fortune(job, city, spouse, numKids)
+{
 document.write("You will be a " + job + " in " + city + ", and married to " + spouse + " with " + numKids + " kids.");
 }
 
 fortune(job[x], city[x], spouse[x], numKids[x])
+
